72d1d2eec659b2ed098733c53c6be8f34bcbf95f
Gabe Blair <gabe@castironcoding.com>
2013-09-04 12:49:55 -0700

    [BUGFIX] Fix useless post-render hook when INT scripts are present
    Fixes: #51748
    Change-Id: Ieb71f606d2633fabd812e83e64f7f285b1fb93f2
    Releases: 6.0, 6.1, 6.2
    
    Conflicts:
    	typo3/sysext/core/Classes/Page/PageRenderer.php

--- typo3/sysext/core/Classes/Page/PageRenderer.php
+++ typo3/sysext/core/Classes/Page/PageRenderer.php
@@ -1932,6 +1932,7 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface {
 	public function render($part = self::PART_COMPLETE) {
 		$this->prepareRendering();
 		list($jsLibs, $jsFiles, $jsFooterFiles, $cssLibs, $cssFiles, $jsInline, $cssInline, $jsFooterInline, $jsFooterLibs) = $this->renderJavaScriptAndCss();
+		$this->executePostRenderHook($jsLibs, $jsFiles, $jsFooterFiles, $cssLibs, $cssFiles, $jsInline, $cssInline, $jsFooterInline, $jsFooterLibs);
 		$metaTags = implode(LF, $this->metaTags);
 		$markerArray = $this->getPreparedMarkerArray($jsLibs, $jsFiles, $jsFooterFiles, $cssLibs, $cssFiles, $jsInline, $cssInline, $jsFooterInline, $jsFooterLibs, $metaTags);
 		$template = $this->getTemplateForPart($part);
@@ -1952,6 +1953,8 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface {
 	 */
 	public function renderPageWithUncachedObjects($substituteHash) {
 		$this->prepareRendering();
+		list($jsLibs, $jsFiles, $jsFooterFiles, $cssFiles, $jsInline, $cssInline, $jsFooterInline, $jsFooterLibs) = $this->renderJavaScriptAndCss();
+		$this->executePostRenderHook($jsLibs, $jsFiles, $jsFooterFiles, $cssFiles, $jsInline, $cssInline, $jsFooterInline, $jsFooterLibs);
 		$markerArray = $this->getPreparedMarkerArrayForPageWithUncachedObjects($substituteHash);
 		$template = $this->getTemplateForPart(self::PART_COMPLETE);
 		return trim(\TYPO3\CMS\Core\Html\HtmlParser::substituteMarkerArray($template, $markerArray, '###|###'));
@@ -2041,7 +2044,6 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface {
 			$jsFooterInline = $jsInline . LF . $jsFooterInline;
 			$jsInline = '';
 		}
-		$this->executePostRenderHook($jsLibs, $jsFiles, $jsFooterFiles, $cssLibs, $cssFiles, $jsInline, $cssInline, $jsFooterInline, $jsFooterLibs);
 		return array($jsLibs, $jsFiles, $jsFooterFiles, $cssLibs, $cssFiles, $jsInline, $cssInline, $jsFooterInline, $jsFooterLibs);
 	}
 
